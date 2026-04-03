9e91cbe907f119d94cb08194c568777c84ade934
sienlong-floenergy <sienlong@floenergy.sg>
2026-04-04 04:22:32 +0800

    Move data loader into correct directories

--- data-loader/src/data_loader/finance/yahoo.py
+++ data-loader/src/data_loader/finance/yahoo.py
@@ -22,7 +22,7 @@ class YahooFinanceLoader(BaseModel):
         '1m', '2m', '5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'
     ] | None = None
 
-    def get_stock_price(
+    def get_stock_price_history(
         self,
         ticker_symbol: str,
         **kwargs
